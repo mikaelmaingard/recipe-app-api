551450dd74446b00056d6399a06a5832c88c760f
Mikael Maingard <mikael@comgrow.org>
2020-12-13 06:11:18 +0200

    Added recipe detail feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -38,3 +38,10 @@ class RecipeSerializer(serializers.ModelSerializer):
             'id', 'title', 'ingredients', 'tags',
             'time_minutes', 'price', 'link')
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail objects"""
+    # detail view shows the actual ingredients and tags
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -46,3 +46,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrieve recipes for the authenticated user"""
         return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        """return appropriate serializer class"""
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+
+        return self.serializer_class
